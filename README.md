3f17190cd4dc487fb44db6d8b4923f4a7902bf2a
Dean Karn <Dean.Karn@gmail.com>
2020-04-28 22:50:07 -0700

    Add Max timeout and Sleep helper (#2)
    This adds:
    - A maximum timeout duration option, non by default.
    - A cancelable sleep helper function.

--- README.md
+++ README.md
@@ -42,6 +42,43 @@ func fallible() error {
 }
 ```
 
+or with cancelable sleep helper
+
+```go
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-playground/backoff-sys"
+)
+
+func main() {
+	bo := backoff.NewExponential().Init()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for i := 0; i < 5; i++ {
+		err := fallible()
+		if err != nil {
+			start := time.Now()
+			if err := bo.Sleep(ctx, i); err != nil {
+				panic(err)
+			}
+			fmt.Printf("Waited %s\n", time.Since(start))
+			continue
+		}
+	}
+}
+
+func fallible() error {
+	return errors.New("failed")
+}
+``` 
+
 License
 ------
 Distributed under MIT License, please see license file in code for more details.
--- exponential.go
+++ exponential.go
@@ -1,6 +1,7 @@
 package backoff
 
 import (
+	"context"
 	"math"
 	"math/rand"
 	"time"
@@ -11,6 +12,7 @@ type ExponentialBuilder struct {
 	factor   float64
 	interval time.Duration
 	jitter   time.Duration
+	max      time.Duration
 }
 
 // NewExponential create a new exponential backoff builder with sane defaults.
@@ -40,12 +42,20 @@ func (e ExponentialBuilder) Jitter(jitter time.Duration) ExponentialBuilder {
 	return e
 }
 
+// Max sets the maximum timeout despite the number of attempts.
+// none/zero is the default.
+func (e ExponentialBuilder) Max(max time.Duration) ExponentialBuilder {
+	e.max = max
+	return e
+}
+
 // Init returns a read-only(thread safe) Exponential backoff entity for use.
 func (e ExponentialBuilder) Init() Exponential {
 	return Exponential{
 		factor:   e.factor,
 		interval: float64(e.interval),
 		jitter:   int64(e.jitter),
+		max:      int64(e.max),
 	}
 }
 
@@ -54,14 +64,29 @@ type Exponential struct {
 	factor   float64
 	interval float64
 	jitter   int64
+	max      int64
 }
 
 // Duration accepts attempt and returns the backoff duration o sleep for.
 func (e Exponential) Duration(attempt int) time.Duration {
 	d := int64(math.Pow(e.factor, float64(attempt)) * e.interval)
-	if e.jitter <= 0 {
-		return time.Duration(d)
+	if e.jitter > 0 {
+		d += rand.Int63n(e.jitter)
 	}
-	jitter := rand.Int63n(e.jitter)
-	return time.Duration(d + jitter)
+	if e.max > 0 && d > e.max {
+		return time.Duration(e.max)
+	}
+	return time.Duration(d)
+}
+
+// Sleep is a convenience function wrapping Duration and allowing the sleep time to be cancelled via the Context.
+func (e Exponential) Sleep(ctx context.Context, attempt int) error {
+	t := time.NewTimer(e.Duration(attempt))
+	defer t.Stop()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-t.C:
+		return nil
+	}
 }
