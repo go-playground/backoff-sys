fcb53fa4833c581e67f81365d96f59c6bf11cce7
Dean Karn <dean.karn@gmail.com>
2023-05-07 08:45:18 -0700

    Add some tests

--- exponential.go
+++ exponential.go
@@ -2,17 +2,23 @@ package backoff
 
 import (
 	"context"
+	"errors"
 	"math"
 	"math/rand"
 	"time"
 )
 
+var (
+	ErrMaxAttemptsReached = errors.New("max attempts reached")
+)
+
 // ExponentialBuilder helps to build the final exponential backoff entity
 type ExponentialBuilder struct {
-	factor   float64
-	interval time.Duration
-	jitter   time.Duration
-	max      time.Duration
+	factor      float64
+	interval    time.Duration
+	jitter      time.Duration
+	max         time.Duration
+	maxAttempts int
 }
 
 // NewExponential create a new exponential backoff builder with sane defaults.
@@ -49,22 +55,30 @@ func (e ExponentialBuilder) Max(max time.Duration) ExponentialBuilder {
 	return e
 }
 
+// MaxAttempts sets the maximum number of attempts before the Sleep(...) function begins returning ErrMaxAttemptsReached.
+func (e ExponentialBuilder) MaxAttempts(max int) ExponentialBuilder {
+	e.maxAttempts = max
+	return e
+}
+
 // Init returns a read-only(thread safe) Exponential backoff entity for use.
 func (e ExponentialBuilder) Init() Exponential {
 	return Exponential{
-		factor:   e.factor,
-		interval: float64(e.interval),
-		jitter:   int64(e.jitter),
-		max:      int64(e.max),
+		factor:      e.factor,
+		interval:    float64(e.interval),
+		jitter:      int64(e.jitter),
+		max:         int64(e.max),
+		maxAttempts: e.maxAttempts,
 	}
 }
 
 // Exponential is the final read-only(thread safe) backoff entity
 type Exponential struct {
-	factor   float64
-	interval float64
-	jitter   int64
-	max      int64
+	factor      float64
+	interval    float64
+	jitter      int64
+	max         int64
+	maxAttempts int
 }
 
 // Duration accepts attempt and returns the backoff duration o sleep for.
@@ -81,7 +95,12 @@ func (e Exponential) Duration(attempt int) time.Duration {
 }
 
 // Sleep is a convenience function wrapping Duration and allowing the sleep time to be cancelled via the Context.
+//
+// This function can also return ErrMaxAttemptsReached if the max attempts have been reached.
 func (e Exponential) Sleep(ctx context.Context, attempt int) error {
+	if e.maxAttempts > 0 && attempt >= e.maxAttempts {
+		return ErrMaxAttemptsReached
+	}
 	t := time.NewTimer(e.Duration(attempt))
 	defer t.Stop()
 	select {
---
+++ exponential_test.go
@@ -0,0 +1,23 @@
+package backoff
+
+import (
+	. "github.com/go-playground/assert/v2"
+
+	"context"
+	"testing"
+	"time"
+)
+
+func TestExponentialBackoff(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	bo := NewExponential().Max(time.Millisecond * 50).Interval(time.Millisecond * 25).Jitter(time.Millisecond * 10).MaxAttempts(5).Init()
+	Equal(t, bo.Sleep(ctx, 0), nil)
+	Equal(t, bo.Sleep(ctx, 1), nil)
+	Equal(t, bo.Sleep(ctx, 2), nil)
+	Equal(t, bo.Sleep(ctx, 3), nil)
+	Equal(t, bo.Sleep(ctx, 4), nil)
+	Equal(t, bo.Sleep(ctx, 5), ErrMaxAttemptsReached)
+
+	cancel()
+	Equal(t, bo.Sleep(ctx, 0), context.Canceled)
+}
